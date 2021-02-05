8ccb29163e8751b5a30c9d7308f8790f5f6ed0a3
Xparham <xparham@gmail.com>
2021-02-05 14:20:10 -0500

    added console log for mail and hub const's

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import { person } from "./archive.js";
 
 const {location:{city, state}} = person;
-const {name:{fName, lName, email, github}} = person ;
+const {name:{fName, lName, mail, hub}} = person ;
 const {orgs:{first, second}} = person;
 const {edu:{high, uni, concen}} = person;
 const {community:{heroes, camp, cares}} = person;
@@ -15,3 +15,6 @@ console.log(`---------`)
 console.log(`My education history includes a HSD at ${high}, and working towards a BFA at ${uni} with a concentration in ${concen}`)
 console.log(`---------`)
 console.log(`I have community service experience with ${heroes} from 2005-2007, ${camp}, from 2007-2012, and ${cares} from 2010-2012.`)
+console.log(`-------------------`)
+console.log(`E-mail:${mail}`)
+console.log(`Github:${hub}`)
