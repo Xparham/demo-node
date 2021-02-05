7e87cccf04d2b002d6fa3125e81e9890f41c0328
Xparham <xparham@gmail.com>
2021-02-05 13:36:18 -0500

    added dotted line in console

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import { person } from "./archive.js";
 
-const { location:{city, state}, name:{fName, lName}, orgs:{first, second}, edu:{high, uni, concen }} = person;
+const { location:{city, state}, name:{fName, lName}, orgs:{first, second}, edu:{high, uni, concen }, community:{heroes, camp, cares}} = person;
 
 console.log( `I am ${fName} ${lName}`)
 console.log(`-------------------`)
@@ -9,3 +9,4 @@ console.log(`---------`)
 console.log(`I am currently in the ${first} with the ${second} `)
 console.log(`---------`)
 console.log(`My education history includes a HSD at ${high}, and working towards a BFA at ${uni} with a concentration in ${concen}`)
+console.log(`---------`)
